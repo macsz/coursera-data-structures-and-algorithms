e8e56615366c5216f51a0bdd2f41103d7e96001d
mszankin <mszankin@nuc196.sn.intel.com>
2017-09-07 16:45:12 -0700

    Solved w5

--- algorithmic-toolbox/w5/edit_distance/edit_distance.py
+++ algorithmic-toolbox/w5/edit_distance/edit_distance.py
@@ -1,7 +1,18 @@
 # Uses python3
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    m, n = len(s), len(t)
+    d = [[0 for i in range(n+1)] for j in range(m+1)] 
+    for i in range(0, m+1):
+        for j in range(0, n+1):
+            if i==0:
+                d[i][j] = j
+            elif j==0:
+                d[i][j] = i
+            elif s[i-1] == t[j-1]:
+                d[i][j] = d[i-1][j-1]
+            else:
+                d[i][j] = 1 + min(d[i][j-1], d[i-1][j], d[i-1][j-1])
+    return d[m][n]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
--- algorithmic-toolbox/w5/primitive_calculator/primitive_calculator.py
+++ algorithmic-toolbox/w5/primitive_calculator/primitive_calculator.py
@@ -1,6 +1,7 @@
 # Uses python3
 import sys
 
+
 def optimal_sequence(n):
     sequence = []
     while n >= 1:
@@ -13,9 +14,61 @@ def optimal_sequence(n):
             n = n - 1
     return reversed(sequence)
 
+
+
+def dp(n):
+#     private static List<Integer> dp_sequence(int n) {
+#         List<Integer> sequence = new ArrayList<Integer>();
+    sequence = []
+#         int [] c = new int[n+1];
+    c = [0] * (n + 1)
+    a1 = 0
+    a2 = 0
+    a3 = 0
+    c[1] = 0
+    for i in range(2, n+1):     
+        a1 = sys.maxsize
+        a2 = sys.maxsize
+        a3 = sys.maxsize
+        if i%2 == 0:
+            a1 = c[int(i/2)] + 1
+        if i%3 == 0:
+            a2 = c[int(i/3)] + 1
+        a3 = c[i-1] + 1
+        m = min(a1, a2, a3)
+        c[i] = m
+    # print(c[n])
+    # for i in reversed(range(2, n+1)):
+    i = n
+    while i > 1:
+        a1 = sys.maxsize
+        a2 = sys.maxsize
+        a3 = sys.maxsize
+        if i%2 == 0:
+            a1 = c[int(i/2)] + 1
+        if i%3 == 0:
+            a2 = c[int(i/3)] + 1
+        a3 = c[i-1]+1
+        m = min(a1, a2, a3)
+        if m == a1:
+            sequence.append(int(i/2))
+            i = int(i/2)
+        elif m == a2:
+            sequence.append(int(i/3))
+            i = int(i/3)
+        else:
+            sequence.append(i-1)
+            i = i-1
+    return reversed(sequence)
+
 input = sys.stdin.read()
 n = int(input)
-sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
+# sequence = list(optimal_sequence(n))
+# print(len(sequence) - 1)
+# for x in sequence:
+#     print(x, end=' ')
+# print()
+sequence = list(dp(n))
+print(len(sequence))
 for x in sequence:
     print(x, end=' ')
